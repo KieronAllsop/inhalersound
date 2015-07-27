2b9a2b8db61e5753dd3fdd1928e3d9e61a5e68c5
Kieron Allsop <kallsop01@qub.ac.uk>
2015-07-27 20:19:38 +0100

    Modifications to wave file handling code

--- inhaler_sound_mark1/wave/wave_file.h
+++ inhaler_sound_mark1/wave/wave_file.h
@@ -1,10 +1,26 @@
 #ifndef WAVE_FILE_H
 #define WAVE_FILE_H
 
-class wave_file
+#include <QObject>
+#include <QFile>
+#include <QAudioFormat>
+
+class wave_file : public QFile
 {
 public:
-    wave_file();
+    wave_file(QObject *parent = 0);
+
+    using QFile::open;
+    bool open(const QString &fileName);
+    const QAudioFormat &fileFormat() const;
+    qint64 headerLength() const;
+
+private:
+    bool readHeader();
+
+private:
+    QAudioFormat m_fileFormat;
+    qint64 m_headerLength;
 };
 
 #endif // WAVE_FILE_H
